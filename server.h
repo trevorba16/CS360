9803db8cbf61993f663b7c6aec2342a9bf9a85a7
trevorba16 <trevor.anderson16@gmail.com>
2016-09-15 13:47:04 -0600

    Added

---
+++ server.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <errno.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include <string>
+
+using namespace std;
+
+class Server {
+public:
+    Server(int port);
+    ~Server();
+
+    void run();
+    
+private:
+    void create();
+    void close_socket()
+    void serve();
+    void handle(int);
+    string get_request(int);
+    void process_request(string);
+    void get_value();
+
+    bool send_response(int, string);
+
+    int port_;
+    int server_;
+    int buflen_;
+    char* buf_;
+    string cache;
+};
